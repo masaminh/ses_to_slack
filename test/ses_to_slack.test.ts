f44f05f229c4fd94501372ffc80ae2d2ad5a8805
masaminh <masami.askw@gmail.com>
2021-08-15 22:24:09 +0900

    S3バケットの属性変更

--- test/ses_to_slack.test.ts
+++ test/ses_to_slack.test.ts
@@ -1,11 +1,27 @@
-import { countResources, expect as expectCDK } from '@aws-cdk/assert';
+import { countResources, expect as expectCDK, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as SesToSlack from '../lib/ses_to_slack-stack';
 
-test('Empty Stack', () => {
+test('Stack', () => {
   const app = new cdk.App();
   // WHEN
   const stack = new SesToSlack.SesToSlackStack(app, 'MyTestStack');
   // THEN
   expectCDK(stack).to(countResources('AWS::SES::ReceiptRule', 1));
+  expectCDK(stack).to(haveResource('AWS::S3::Bucket', {
+    LifecycleConfiguration: {
+      Rules: [
+        {
+          ExpirationInDays: 30,
+          Status: 'Enabled',
+        },
+      ],
+    },
+    PublicAccessBlockConfiguration: {
+      BlockPublicAcls: true,
+      BlockPublicPolicy: true,
+      IgnorePublicAcls: true,
+      RestrictPublicBuckets: true,
+    },
+  }));
 });
