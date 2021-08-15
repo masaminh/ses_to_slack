82ac7f4767ee350a754d2fc425bee61bf9fe47b3
masaminh <33191610+masaminh@users.noreply.github.com>
2021-08-15 09:26:12 +0900

    添付画像対応 (#3)

--- test/lambda/message_formatter.test.ts
+++ test/lambda/message_formatter.test.ts
@@ -2,14 +2,21 @@ import fs from 'fs';
 import MessageFormatter from '../../lib/lambda/message_formatter';
 
 describe('MessageFormatter', () => {
+  jest.setTimeout(10000);
+
   test.each`
     sourceFile                  | expectedFile
-    ${'testdata/message-1.eml'} | ${'testdata/message-1-expected.txt'}
-    ${'testdata/message-2.eml'} | ${'testdata/message-2-expected.txt'}
+    ${'testdata/message-1.eml'} | ${'testdata/message-1-expected.json'}
+    ${'testdata/message-2.eml'} | ${'testdata/message-2-expected.json'}
+    ${'testdata/message-3.eml'} | ${'testdata/message-3-expected.json'}
   `('$source', async ({ sourceFile, expectedFile }) => {
     const readStream = fs.createReadStream(sourceFile);
     const actual = await MessageFormatter.format(readStream);
-    const expected = fs.readFileSync(expectedFile, 'utf8');
-    expect(actual).toBe(expected);
+    const expected = JSON.parse(fs.readFileSync(expectedFile, 'utf8'));
+    expect(actual.text).toBe(expected.text);
+    const actualAttachments = actual.attachments.map(
+      (x) => ({ contentType: x.contentType, fileName: x.fileName }),
+    );
+    expect(actualAttachments).toEqual(expected.attachments);
   });
 });
